e28ed43d8320116793241a3d24f77bf010dd5517
Max Wang <maxwang051@gmail.com>
2017-04-22 12:02:35 -0500

    added ejs and uncommented passport stuff

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'database': 'mongodb://earthack:1234@ds111851.mlab.com:11851/earthack'
+    'database': 'mongodb://earthack:1234@ds111851.mlab.com:11851/earthack',
 
     'googleAuth': {
         'clientId' : '378002092970-0fbe0n2kpbncirg0jl3nkqc4q43u5age.apps.googleusercontent.com',
