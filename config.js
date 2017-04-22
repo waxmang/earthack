f302ecd2bab9f468a07287c6a23562cb899e6652
Max Wang <maxwang051@gmail.com>
2017-04-22 10:46:08 -0500

    added polar and google stuff

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 module.exports = {
     'database': 'mongodb://earthack:1234@ds111851.mlab.com:11851/earthack'
+
+    'googleAuth': {
+        'clientId' : '378002092970-0fbe0n2kpbncirg0jl3nkqc4q43u5age.apps.googleusercontent.com',
+        'clientSecret' : '4jDfvsuTbCMLzi1LiFIbBd5x',
+        'callbackURL' : 'http://localhost:8080/auth/google/callback'
+    }
 }
