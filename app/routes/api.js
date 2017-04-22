e5c71595810931b3a236c23302c5a066c67ce630
Lydia Yu <lydiayu@utexas.edu>
2017-04-22 13:42:15 -0600

    Added routes

--- app/routes/api.js
+++ app/routes/api.js
@@ -18,5 +18,119 @@ module.exports =  function(app,express,passport){
         });
     });
 
+
+    apiRouter.route('/user/:user_id')
+
+    	.get(function(req,res){
+    		User.findOne({'id':req.params.user_id},function(err,user){
+    			if (err) return res.send(err);
+    			//return that user
+    			res.json(user);
+    		});
+    	});
+
+    	.put(function(req,res){
+    		User.findOne({'id':req.params.user_id},function(err,user){
+    			if (err) return res.send(err);
+    			
+    			//update info
+    			if (req.body.name) user.name = req.body.name;
+    			if (req.body.userType) user.userType = req.body.userType;
+    			if (req.body.location) user.location = req.body.location;
+
+    			//save the user
+    			user.save(function(err){
+    				if (err) return res.send(err);
+
+    				//return update message
+    				res.json({message: 'User updated!'});
+    			});
+
+    		})
+    	})
+
+    apiRouter.route('/user/:user_id/entries')
+
+    	//add entry for user
+    	.post(function(req,res){
+    		var entry = new Entry();
+
+    		entry.restaurant_id = req.params.user_id;
+    		entry.food_category = req.body.food_category;
+    		entry.food_items = req.body.food_items;
+    		entry.status = req.body.status;
+
+    		entry.save(function(err){
+    			if (err) {
+    				return res.send(err);
+    			}
+    			//return entry
+    			return res.json(entry);
+    		});
+    	})
+
+    	//get all entries for user
+    	.get(function(req,res){
+    		Entry.find({'restaurant_id':req.params.user_id},function(err,entries){
+    			if (err) return res.send(err);
+    			//return entries of that user
+    			res.json(entries);
+    		});
+    	});
+
+    apiRouter.route('/user/:user_id/entries/:entry_id')
+
+    	.get(function(req,res){
+    		Entry.findbyId(req.params.entry_id,function(err,entry){
+    			if (err) res.send(err);
+
+    			//make sure user is logged in
+    			if (req.user != null && req.user.id == entry.restaurant_id){
+    				res.json(entry);
+    			} else {
+    				res.json({message: "You don't have access to this note"});
+    			}
+    		});
+    	})
+
+    	.put(function(req,res){
+    		Entry.findbyId(req.params.entry_id,function(err,note){
+    			if (err) res.send(err);
+
+    			if (req.user != null && req.user.id == entry.restaurant_id){
+    				if (req.body.restaurant_id) entry.restaurant_id = req.body.restaurant_id;
+    				if (req.body.food_category) entry.food_category = req.body.food_category;
+    				if (req.body.food_items) entry.food_items = req.body.food_items;
+    				if (req.body.status) entry.status = req.body.status;
+
+    				entry.save(function(err){
+    					if (err) {
+    						return res.send(err);
+    					}	else{
+    						return res.json(message:"Entry updated!");
+    					}
+    				});
+    			}
+    		})
+    	})
+
+    	.delete(function(req,res){
+    		var id = req.params.entry_id;
+    		Entry.remove({
+    			_id: id
+    		}, function(err,note){
+    			if (err)
+    				res.send(err);
+    			res.json({message:'Successfully deleted!'});
+    		});
+    	});
+
+    // Get all entries
+    apiRouter.get('/allEntries', function(req, res) {
+        Entry.find({}, function(err, data) {
+            res.json(data);
+        });
+    });
+
     return apiRouter;
 };
