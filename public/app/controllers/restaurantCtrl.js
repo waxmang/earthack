eed81cb5046a3dcf5075ed9dcf0b6cab63b6bfa3
Max Wang <maxwang051@gmail.com>
2017-04-22 16:09:46 -0500

    not sure

--- public/app/controllers/restaurantCtrl.js
+++ public/app/controllers/restaurantCtrl.js
@@ -1,9 +1,8 @@
 angular.module('restaurantCtrl',[])
 
-.controller('restaurantController',['$scope','$http','$location', function($scope,$http,$location){
-	
+.controller('restaurantController',['$scope','$http','$location', function($scope, $http, $location){
+
 	$scope.user = "Max";
 	$scope.allEntries = [];
-	
 
 }]);
